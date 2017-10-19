98020c48f14cbe989680434d8daecc4e2117c558
Michael Fournier <4niermike@gmail.com>
2017-10-19 11:35:44 -0400

    Add mentor availability method

--- lib/kele.rb
+++ lib/kele.rb
@@ -19,6 +19,11 @@ class Kele
     JSON.parse(response.body)
   end
 
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(base_url("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
+    @mentor_avail = JSON.parse(response.body)
+  end
+
   def get_roadmap
   end
 
