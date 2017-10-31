b6080a50459eeb937521d54748b805287c5021e2
Michael Fournier <4niermike@gmail.com>
2017-10-31 13:49:59 -0400

    fixed minor omissions

--- lib/kele.rb
+++ lib/kele.rb
@@ -37,7 +37,7 @@ class Kele
   end
 
   def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
-    response = self.class.post(base_url("checkpoint_submissions"), body: {"assignment_branch" = assignment_branch, "assignment_commit_link" = assignment_commit_link, "checkpoint_id" = checkpoint_id, "comment" = comment, "enrollment_id" = enrollment_id}, headers: {"authorization" = @auth_token})
+    response = self.class.post(base_url("checkpoint_submissions"), body: {"assignment_branch" => assignment_branch, "assignment_commit_link" => assignment_commit_link, "checkpoint_id" => checkpoint_id, "comment" => comment, "enrollment_id" => enrollment_id}, headers: {"authorization" => @auth_token})
     puts 'Your submission has been processed!' if response.success?
   end
 
